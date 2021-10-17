a3064d3207284e4d3f98387eda85fb7a8c5bfcca
wassil <ouassil.dahimene@gmail.com>
2021-10-17 19:56:42 +0100

    first commit

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -10,16 +10,12 @@ const Main = ({ children, router }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Ouassil's homepage" />
         <meta name="author" content="Ouassil Dahimene" />
-        <meta name="author" content="craftzdog" />
+        <meta name="author" content="ouassildahimene" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@craftzdog" />
-        <meta name="twitter:creator" content="@craftzdog" />
-        <meta name="twitter:image" content="/card.png" />
         <meta property="og:site_name" content="Ouassil Dahimene's Homepage" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="/card.png" />
+        <meta property="og:image" content="/profil.jpeg" />
         <title>Ouassil Dahimene - Homepage</title>
       </Head>
 
